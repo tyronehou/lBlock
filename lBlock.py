429865ffdf92024b5b6ba58e1d10b5599e272f28
tyronehou <tyronehou@wireless1x-155-41-122-220.bu.edu>
2015-04-12 00:02:19 -0400

    Finished lBlock.py
    -added getDiagonal & getDiagonals methods
    -added overridden draw method
    -added helper methods
    -added unit test

--- lBlock.py
+++ lBlock.py
@@ -1,14 +1,39 @@
 # File: lBlock.py
 # Author: Tyrone Hou (tyroneh@bu.edu)
 # Purpose: ADT representing l-shaped blocks
-
 from graphics import *
 
-(L,R,U,D) = (-1,1,1,-1)
+directions = {
+    1: (1, 1),
+    2: (-1, 1),
+    3: (-1, -1),
+    4: (1, -1)
+}
+
+# Helper functions
+
+def eraseLine(p1, p2, win):
+    """ Draws over a line by covering it with a white line
+        p1 & p2 are (x,y) tuples rep. the endpoints
+    """
+    l = Line(p1, p2)
+    l.setOutline('white')
+    l.draw(win)
+    
+
+def getDiagonal(x, y, d, s):
+    """ Calculates the endpoint of a diagonal of
+        a square of side length s given the other endpoint and the
+        direction the endpoint faces. Returns a double value.
+        d -- direction vector
+        corner -- (x,y) coordinate tuple """
+
+    dx, dy = d
+    return (x + s*dx, y + s*dy)
 
 
-class lBlock(Polygon):
-    def __init__(self, x, y, orientation, length):
+class lBlock:
+    def __init__(self, x, y, orient, length):
         """ Defines a unique block on a Cartesian plane from
             x, y coords and a given orientation.
                     x,y -- x and y coords of the smallest square that circumscribes the lBlock.
@@ -19,18 +44,73 @@ class lBlock(Polygon):
         
         self.x = x
         self.y = y
-        self.length = length
-        self.orientation = orientation
-        Polygon.__init__(self.calcPoints())
-        
-    def calcPoints(self):
-        """ Calculates and returns a list of the vertices of the L-shaped tile"""
+        self.center = Point(x, y)
+        self.length = length          
+        self.orientation = directions[orient]
 
-        vertices = []
-        
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if (i, j) is not orientation:
-                    vertices += [( self.x+(length*i), self.y+(length*j) )]
 
-        return vertices
+    def __str__(self):
+        length = "Length: " + str(self.length)
+        center = "Center: (" + str(self.x) + ", " + str(self.y) + ")"
+        orientation = "Orientation: " + str(self.orientation)
+        
+        return length + "\n" + center + "\n" + orientation + "\n"
+
+    
+    def draw(self, win):
+        diagonals = self.getDiagonals()
+
+        # Draws three minisquares to get the form of the blocks
+        for p1, p2 in diagonals:
+            Rectangle(p1, p2).draw(win)
+            
+        # Erases the two extraneous inner lines
+        dx, dy = self.orientation
+        p1 = Point(self.x - (dx * self.length), self.y) # endpoint 1
+        q1 = Point(self.x, self.y - (dy * self.length)) # endpoint 2
+
+        eraseLine(self.center, p1, win)
+        eraseLine(self.center, q1, win)
+        
+        
+    def getDiagonals(self):
+        """ Calculates and returns the endpoints of a diagonal
+            of each of the 3 subsquares of the tile"""
+        
+        diagonals = []
+        for i in range(1,5):
+                d = directions[i] # possible orientation
+                
+                if d != self.orientation:
+                    diagonal = getDiagonal(self.x, self.y, d, self.length)
+                    Dx, Dy = diagonal
+
+                    diagonals += [(self.center, Point(Dx, Dy))]
+                                
+        return diagonals # returns the vertices of the lBlock
+
+
+def test():
+    print("Initializing lBlock...")
+    block = lBlock(100, 100, 1, 50)
+    print(block)
+    
+    print("Testing getCenters:")
+    print("(50, 150) (50, 50) (150, 50)")
+    for diag in block.getDiagonals():
+        p = diag[1]
+        x, y = p.getX(), p.getY()
+        print("(" + str(x) + ", " + str(y) + ")", end = " ")
+
+    print()
+        
+    print("Drawing lBLock...")
+    win = GraphWin()
+    block.draw(win)
+    win.getMouse()
+    win.close()
+
+if __name__ == '__main__':
+    test()
+
+    
